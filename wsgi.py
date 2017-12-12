462f09395dff8c98379e492d1fd11f7799d7f77e
FrobozzElectric <gpfau7@gmail.com>
2017-12-12 18:32:21 -0500

    log filter

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,16 @@
+import logging
 from app import app
+
+
+class LogFilter(logging.Filter):
+    def filter(self, record):
+        if '/_healthcheck' in record.getMessage():
+            return False
+        return True
+
+
+logger = logging.getLogger('gunicorn.access')
+logger.addFilter(LogFilter())
 
 if __name__ == '__main__':
     app.run()
